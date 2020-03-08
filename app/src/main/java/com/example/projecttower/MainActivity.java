03ba35d45027fa2a322cb062e1be78ef90d8cdf2
youssefelmougy <youssefelmougy@yahoo.com>
2020-03-08 17:39:20 -0400

    Added GUI for log in and sign up

--- app/src/main/java/com/example/projecttower/MainActivity.java
+++ app/src/main/java/com/example/projecttower/MainActivity.java
@@ -15,16 +15,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+       // setContentView(R.layout.activity_main);
+        //setContentView(R.layout.home_screen);
+        setContentView(R.layout.login_signup_screen);
+       // setContentView(R.layout.sign_up_screen);
+
         // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+         //menu should be considered as top level destinations.
+         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+               R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+              .build();
+
+
     }
 
 }
